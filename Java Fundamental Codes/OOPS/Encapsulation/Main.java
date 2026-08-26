98224609b8ddbe840c77273631d42599088dcc53
Devashish Dey <kumardevofficial@gmail.com>
2026-08-26 23:01:47 +0530

    add more class in encapsulation code

--- Java Fundamental Codes/OOPS/Encapsulation/Main.java
+++ Java Fundamental Codes/OOPS/Encapsulation/Main.java
@@ -26,19 +26,69 @@ class Student{
  }
 }
 
+class BankAccount{
+	private int accountNumber;
+	private String accountHolderName;
+	private double balance;
+	
+	public int getAccountNumber(){
+		return this.accountNumber;
+	}
+	
+	public String getAccountHolderName(){
+		return this.accountHolderName;
+	}
+	
+	public double getBalance(){
+		return this.balance;
+	}
+	
+	public void deposit(double amount){
+		this.balance = balance + amount;
+	}
+	
+	public void withdraw(double withdraw){
+		if(balance >= withdraw){
+			this.balance = balance - withdraw;
+		} else {
+			System.out.println("You do not have a sufficient balance");
+		}
+	}
+	
+	public void setAccountNumber(int accountNumber){
+		this.accountNumber = accountNumber;
+	}
+	public void setAccountHolderName(String accountHolderName){
+		this.accountHolderName = accountHolderName;
+	}
+	
+}
+
 
 
 
 public class Main{
 
  public static void main(String[] args){
- Student st = new Student();
- st.setName("Birbal");
- st.setAge(35);
- st.setRollNumber(23);
+ //Student st = new Student();
+ //st.setName("Birbal");
+ //st.setAge(35);
+ //st.setRollNumber(23);
+ //
+ //st.getName();
+ //st.getAge();
+ //st.getRollNumber();
  
- st.getName();
- st.getAge();
- st.getRollNumber();
+ BankAccount ac = new BankAccount();
+ ac.setAccountNumber(64859375);
+ ac.setAccountHolderName("Vikash Pathak");
+ ac.deposit(5500.60);
+ 
+ System.out.println(ac.getAccountHolderName());
+ System.out.println(ac.getAccountNumber());
+ System.out.println(ac.getBalance());
+ ac.withdraw(600);
+ System.out.println(ac.getBalance());
+ 
  }
 }
