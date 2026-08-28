8d2cf64b4501bbbd9b575466e9c978446484d34a
Devashish Dey <kumardevofficial@gmail.com>
2026-08-28 08:35:48 +0530

    Day 4 class practice

---
+++ Day-4/Account.java
@@ -0,0 +1,12 @@
+class AccountInfo
+{
+ public static void main(String args[])
+ {
+ System.out.println("Account Holder : Kodewala");
+ System.out.println("Type : Current");
+ System.out.println("Address : BTM Bangalore");
+ System.out.println("Bank Name : SBI");
+ }
+ 
+ 
+}
