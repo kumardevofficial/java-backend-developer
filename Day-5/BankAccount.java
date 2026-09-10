ed87a0e3b6b235425402bdf0f48d1f15acb95bf7
Devashish Dey <kumardevofficial@gmail.com>
2026-09-10 21:57:22 +0530

    added new file in day 5 folder

---
+++ Day-5/BankAccount.java
@@ -0,0 +1,13 @@
+class BankAccount 
+{
+ public static void main(String args[])
+ {
+  String employeeName = args[0];
+  String accountType = args[1];
+  String branchName = args[2];
+  
+  System.out.println("Employee Name : " + employeeName);
+  System.out.println("Account Type :"+ accountType);
+  System.out.println("Branch Name :"+ branchName);
+ }
+}
---
+++ Day-5/StudentDetails.java
@@ -0,0 +1,13 @@
+class StudentDetails
+{
+    public static void main(String args[])
+    {
+        String studentName = args[0];
+        String studentAdd = args[1];
+        String studentSection = args[2];
+
+        System.out.println("Name of the studetn:"+studentName);
+        System.out.println("Address of the student:"+studentAdd);
+        System.out.println("Section of the student:"+studentSection);
+    }
+}
