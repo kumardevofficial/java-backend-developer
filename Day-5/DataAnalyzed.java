19158f45123fe848ad8703544377763307e84664
Devashish Dey <kumardevofficial@gmail.com>
2026-09-10 16:14:34 +0530

    day 5

---
+++ Day-5/DataAnalyzed.java
@@ -0,0 +1,8 @@
+class DataAnalyzed
+{
+ public static void main(String args[])
+ {
+  String fileName = args[0];
+  System.out.println("we are analyzing your file"+" "+fileName+" "+"please wait....");
+ }
+}
---
+++ Day-5/OrderDetails.java
@@ -0,0 +1,21 @@
+class OrderDetails
+{
+ public static void main(String args[])
+ {
+  String orderId = args[0];
+  String productName = args[1];
+  String placedBy = args[2];
+  String deliveryAdd = args[3];
+  
+  System.out.println(orderId);
+  System.out.println(productName);
+  System.out.println(placedBy);
+  System.out.println(deliveryAdd);
+ }
+}
+
+//Input Details
+//ORD11234
+//IPhone16
+//Devashish
+//2nd Stage, BTM Layout Banglore
