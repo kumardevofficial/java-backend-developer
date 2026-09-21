41a306e099247a27df34bebb1ad00d62b037cb24
Devashish Dey <kumardevofficial@gmail.com>
2026-09-21 18:52:35 +0530

    rename all file and also add day_12 practice

---
+++ Day_12/src/com/amazon/sample/HelloWorld.java
@@ -0,0 +1,17 @@
+package com.amazon.sample;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+	
+		System.out.println("Hello world");
+		HelloWorld.helloWorld();
+
+	}
+	
+	static void helloWorld()
+	{
+		System.out.println("HelloWorld.helloWorld()");
+	}
+
+}
